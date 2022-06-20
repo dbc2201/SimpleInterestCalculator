807b6a9fd6903557abb6de9de0d05311a5566b6e
Divyansh Bhardwaj <divyanshb22@gmail.com>
2022-06-20 22:38:40 +0530

    Update SimpleInterestCalculatorTest.java

--- src/test/java/io/github/dbc/SimpleInterestCalculatorTest.java
+++ src/test/java/io/github/dbc/SimpleInterestCalculatorTest.java
@@ -7,22 +7,28 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class SimpleInterestCalculatorTest {
 
-    private SimpleInterestCalculator simpleInterestCalculator;
+	private SimpleInterestCalculator simpleInterestCalculator;
 
-    @BeforeEach
-    void setUp() {
-        simpleInterestCalculator = new SimpleInterestCalculator();
-    }
+	@BeforeEach
+	void setUp() {
+		simpleInterestCalculator = new SimpleInterestCalculator();
+	}
 
-    @ParameterizedTest(name = "calculateSimpleInterest({arguments})")
-    @CsvSource(value = {"10000.0f,10.0f,1.0f", "100000.0f,12.8f,10.5f", "1000.0f,0.5f,0.6f", "12987.5f,14.5f,8.9f"})
-    void calculateSimpleInterest(float principle, float rate, float time) {
-        float calculatedSimpleInterest = simpleInterestCalculator.calculateSimpleInterest(principle, rate, time);
-        Assertions.assertEquals(
-                (principle * rate * time) / 100.0f,
-                calculatedSimpleInterest,
-                0.1f,
-                "The calculated simple interest is not correct!"
-        );
-    }
+	@ParameterizedTest(name = "calculateSimpleInterest({arguments})")
+	@CsvSource(value = {
+			"2063000.0f,37.8f,19.0f,14816466.0f",
+			"8646000.0f,8.9f,12.0f,9233928.0f",
+			"4660000.0f,32.6f,25.0f,37979000.0f",
+			"129870.0f,14.5f,9.0f,169480.36f",
+			"528000.0f,17.0f,30.0f,2692800.0f"
+	})
+	void calculateSimpleInterest(float principle, float rate, float time, float expectedInterest) {
+		float calculatedSimpleInterest = simpleInterestCalculator.calculateSimpleInterest(principle, rate, time);
+		Assertions.assertEquals(
+				expectedInterest,
+				calculatedSimpleInterest,
+				0.1,
+				"The calculated simple interest is not correct!"
+		);
+	}
 }
